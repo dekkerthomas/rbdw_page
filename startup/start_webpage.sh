a96f813d63a482d161d27342702d2712d4f78d53
thomas <Thomas12dekker@gmail.com>
2024-08-10 21:42:32 +0200

    main

---
+++ startup/start_webpage.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Disable screensaver and power management
+xset s off
+xset s noblank
+xset -dpms
+
+# Wait for the system to fully boot
+sleep 25
+
+# Launch Chromium in kiosk mode (full-screen) and open the desired webpage
+chromium-browser --noerrdialogs --disable-infobars --kiosk https://rbdwp2000.dekkerthomas.com/
