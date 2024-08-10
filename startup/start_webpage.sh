8f789a63969c2e31a7f6bcd07ca1c23e8402d949
thomas <Thomas12dekker@gmail.com>
2024-08-10 22:03:50 +0200

    m

--- startup/start_webpage.sh
+++ startup/start_webpage.sh
@@ -6,7 +6,7 @@ xset s noblank
 xset -dpms
 
 # Wait for the system to fully boot
-sleep 25
+sleep 10
 
 # Launch Chromium in kiosk mode (full-screen) and open the desired webpage
 chromium-browser --noerrdialogs --disable-infobars --kiosk https://rbdwp2000.dekkerthomas.com/
